16197094065e4dd4a79c546be839c770d8ae2743
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-02-06 16:46:23 +0000

    added high scores javascript

---
+++ assets/js/highscore.js
@@ -0,0 +1,16 @@
+const highScores = JSON.parse(localStorage.getItem('highScores')) || undefined;
+const highScoresList = document.getElementById('highScoresList');
+
+
+function mapScores() {
+    if (!highScores) {
+        highScoresList.innerHTML = 'There are no scores!';
+    } else {
+        highScoresList.innerHTML = highScores.map(score => {
+            return `<li class="high-score">${score.name} - ${score.score}</li>`;
+        })
+        .join("");
+    }
+}
+
+mapScores();
