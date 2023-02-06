26207c4556bb1a7d05a6359dce0bddb89b73f69e
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-02-06 16:20:03 +0000

    started high scores page

--- assets/js/end.js
+++ assets/js/end.js
@@ -6,3 +6,26 @@ const maxHighScore = 5;
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+
+function saveHighScore(event) {
+    event.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(maxHighScore);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign('index.html');
+}
+
