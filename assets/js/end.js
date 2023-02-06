756de626f79340a2e74a3f143991dcaf8d2f80ff
D0nie1998 <115627417+D0nie1998@users.noreply.github.com>
2023-02-06 17:14:35 +0000

    fixed js problem on end page

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,10 +1,11 @@
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const username = document.getElementById('username');
 const maxHighScore = 5;
 
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 
 finalScore.innerText = mostRecentScore;
 
